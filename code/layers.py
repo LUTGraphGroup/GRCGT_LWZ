ea10ec6119cade9a668dc3880880cfdfc1355952
LUTGraphGroup <109469869+LUTGraphGroup@users.noreply.github.com>
2024-06-19 10:01:33 +0800

    Update layers.py

--- code/layers.py
+++ code/layers.py
@@ -4,109 +4,109 @@ import torch.nn.functional as F
 from math import log
 from typing import Optional, Tuple
 import torch
-# from torch import Tensor
-# from torch.nn import Parameter
-# from torch_sparse import SparseTensor, matmul
-# from torch_geometric.nn.conv import MessagePassing
-# from torch_geometric.nn.conv.gcn_conv import gcn_norm
-# from torch_geometric.typing import Adj, OptTensor
-# from utils import glorot
+from torch import Tensor
+from torch.nn import Parameter
+from torch_sparse import SparseTensor, matmul
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
+from torch_geometric.typing import Adj, OptTensor
+from utils import glorot
 
 
-# class GCN2Conv(MessagePassing):
-#     _cached_edge_index: Optional[Tuple[Tensor, Tensor]]
-#     _cached_adj_t: Optional[SparseTensor]
-#
-#     def __init__(self, channels: int, alpha: float, theta: float = None,
-#                  layer: int = None, shared_weights: bool = True,
-#                  cached: bool = False, add_self_loops: bool = True,
-#                  normalize: bool = True, **kwargs):
-#
-#         kwargs.setdefault('aggr', 'add')
-#         super().__init__(**kwargs)
-#
-#         self.channels = channels
-#         self.alpha = alpha
-#         self.beta = 1.
-#         if theta is not None or layer is not None:
-#             assert theta is not None and layer is not None
-#             self.beta = log(theta / layer + 1)
-#         self.cached = cached
-#         self.normalize = normalize
-#         self.add_self_loops = add_self_loops
-#
-#         self._cached_edge_index = None
-#         self._cached_adj_t = None
-#
-#         self.weight1 = Parameter(torch.Tensor(channels, channels))
-#
-#         if shared_weights:
-#             self.register_parameter('weight2', None)
-#         else:
-#             self.weight2 = Parameter(torch.Tensor(channels, channels))
-#
-#         self.reset_parameters()
-#
-#     def reset_parameters(self):
-#         glorot(self.weight1)
-#         glorot(self.weight2)
-#         self._cached_edge_index = None
-#         self._cached_adj_t = None
-#
-#     def forward(self, x: Tensor, x_0: Tensor, edge_index: Adj,
-#                 edge_weight: OptTensor = None) -> Tensor:
-#         """"""
-#
-#         if self.normalize:
-#             if isinstance(edge_index, Tensor):
-#                 cache = self._cached_edge_index
-#                 if cache is None:
-#                     edge_index, edge_weight = gcn_norm(  # yapf: disable
-#                         edge_index, edge_weight, x.size(self.node_dim), False,
-#                         self.add_self_loops, self.flow, dtype=x.dtype)
-#                     if self.cached:
-#                         self._cached_edge_index = (edge_index, edge_weight)
-#                 else:
-#                     edge_index, edge_weight = cache[0], cache[1]
-#
-#             elif isinstance(edge_index, SparseTensor):
-#                 cache = self._cached_adj_t
-#                 if cache is None:
-#                     edge_index = gcn_norm(  # yapf: disable
-#                         edge_index, edge_weight, x.size(self.node_dim), False,
-#                         self.add_self_loops, self.flow, dtype=x.dtype)
-#                     if self.cached:
-#                         self._cached_adj_t = edge_index
-#                 else:
-#                     edge_index = cache
-#
-#         # propagate_type: (x: Tensor, edge_weight: OptTensor)
-#         x = self.propagate(edge_index, x=x, edge_weight=edge_weight, size=None)
-#
-#         x.mul_(1 - self.alpha)
-#         x_0 = self.alpha * x_0[:x.size(0)]
-#
-#         if self.weight2 is None:
-#             out = x.add_(x_0)
-#             out = torch.addmm(out, out, self.weight1, beta=1. - self.beta,
-#                               alpha=self.beta)
-#         else:
-#             out = torch.addmm(x, x, self.weight1, beta=1. - self.beta,
-#                               alpha=self.beta)
-#             out = out + torch.addmm(x_0, x_0, self.weight2,
-#                                     beta=1. - self.beta, alpha=self.beta)
-#
-#         return out
-#
-#     def message(self, x_j: Tensor, edge_weight: OptTensor) -> Tensor:
-#         return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
-#
-#     def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:
-#         return matmul(adj_t, x, reduce=self.aggr)
-#
-#     def __repr__(self) -> str:
-#         return (f'{self.__class__.__name__}({self.channels}, '
-#                 f'alpha={self.alpha}, beta={self.beta})')
+class GCN2Conv(MessagePassing):
+    _cached_edge_index: Optional[Tuple[Tensor, Tensor]]
+    _cached_adj_t: Optional[SparseTensor]
+
+    def __init__(self, channels: int, alpha: float, theta: float = None,
+                 layer: int = None, shared_weights: bool = True,
+                 cached: bool = False, add_self_loops: bool = True,
+                 normalize: bool = True, **kwargs):
+
+        kwargs.setdefault('aggr', 'add')
+        super().__init__(**kwargs)
+
+        self.channels = channels
+        self.alpha = alpha
+        self.beta = 1.
+        if theta is not None or layer is not None:
+            assert theta is not None and layer is not None
+            self.beta = log(theta / layer + 1)
+        self.cached = cached
+        self.normalize = normalize
+        self.add_self_loops = add_self_loops
+
+        self._cached_edge_index = None
+        self._cached_adj_t = None
+
+        self.weight1 = Parameter(torch.Tensor(channels, channels))
+
+        if shared_weights:
+            self.register_parameter('weight2', None)
+        else:
+            self.weight2 = Parameter(torch.Tensor(channels, channels))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        glorot(self.weight1)
+        glorot(self.weight2)
+        self._cached_edge_index = None
+        self._cached_adj_t = None
+
+    def forward(self, x: Tensor, x_0: Tensor, edge_index: Adj,
+                edge_weight: OptTensor = None) -> Tensor:
+        """"""
+
+        if self.normalize:
+            if isinstance(edge_index, Tensor):
+                cache = self._cached_edge_index
+                if cache is None:
+                    edge_index, edge_weight = gcn_norm(  # yapf: disable
+                        edge_index, edge_weight, x.size(self.node_dim), False,
+                        self.add_self_loops, self.flow, dtype=x.dtype)
+                    if self.cached:
+                        self._cached_edge_index = (edge_index, edge_weight)
+                else:
+                    edge_index, edge_weight = cache[0], cache[1]
+
+            elif isinstance(edge_index, SparseTensor):
+                cache = self._cached_adj_t
+                if cache is None:
+                    edge_index = gcn_norm(  # yapf: disable
+                        edge_index, edge_weight, x.size(self.node_dim), False,
+                        self.add_self_loops, self.flow, dtype=x.dtype)
+                    if self.cached:
+                        self._cached_adj_t = edge_index
+                else:
+                    edge_index = cache
+
+        # propagate_type: (x: Tensor, edge_weight: OptTensor)
+        x = self.propagate(edge_index, x=x, edge_weight=edge_weight, size=None)
+
+        x.mul_(1 - self.alpha)
+        x_0 = self.alpha * x_0[:x.size(0)]
+
+        if self.weight2 is None:
+            out = x.add_(x_0)
+            out = torch.addmm(out, out, self.weight1, beta=1. - self.beta,
+                              alpha=self.beta)
+        else:
+            out = torch.addmm(x, x, self.weight1, beta=1. - self.beta,
+                              alpha=self.beta)
+            out = out + torch.addmm(x_0, x_0, self.weight2,
+                                    beta=1. - self.beta, alpha=self.beta)
+
+        return out
+
+    def message(self, x_j: Tensor, edge_weight: OptTensor) -> Tensor:
+        return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
+
+    def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:
+        return matmul(adj_t, x, reduce=self.aggr)
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.channels}, '
+                f'alpha={self.alpha}, beta={self.beta})')
 
 
 def init_params(module, n_layers):
